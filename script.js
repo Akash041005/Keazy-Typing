64dc460d6104cba73854660c17c5ff4394683abd
Akash041005 <akash.choppadandi04@gmail.com>
2025-04-23 10:28:51 +0000

    almost done 1

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ naame.addEventListener("keydown", function(e) {
       let alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 
-      let x = Math.floor(Math.random()*alphabets.length+1)
+      let x = Math.floor(Math.random()*alphabets.length)
       y = alphabets[x];
       console.log(y);
       genc.textContent=`${y}`
